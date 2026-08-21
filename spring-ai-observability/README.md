3387cddd071579ac81af19953bac5103a6a879f3
Fu Cheng <alexcheng1982@gmail.com>
2026-08-21 18:10:57 +1200

    add new project

---
+++ spring-ai-observability/README.md
@@ -0,0 +1,7 @@
+# Spring AI Observability
+
+Spring AI Observability using [Langfuse](https://langfuse.com/) and [MLflow](https://mlflow.org)
+
+[![build](https://github.com/JavaAIDev/spring-ai-observability/actions/workflows/build.yaml/badge.svg?branch=main)](https://github.com/JavaAIDev/spring-ai-observability/actions/workflows/build.yaml)
+
+After starting the Spring AI application, access the chat UI at `http://localhost:8080/webjars/chat-agent-ui/index.html`.
---
+++ spring-ai-observability/src/main/java/com/javaaidev/springai/observability/AppConfig.java
@@ -0,0 +1,53 @@
+package com.javaaidev.springai.observability;
+
+import com.javaaidev.springai.observability.otel.ChatModelCompletionContentObservationFilter;
+import java.util.Arrays;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.ai.chat.client.ChatClientBuilderCustomizer;
+import org.springframework.ai.mcp.AsyncMcpToolCallbackProvider;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+@Configuration
+public class AppConfig {
+
+  @Bean
+  public ApplicationRunner startupRunner(
+      final McpAsyncClientInitializer mcpAsyncClientInitializer) {
+    return args -> mcpAsyncClientInitializer.runAsyncTask();
+  }
+
+  @Bean
+  public ChatClientBuilderCustomizer chatClientCustomizer(
+      final AsyncMcpToolCallbackProvider toolCallbackProvider) {
+    return builder -> builder.defaultTools(toolCallbackProvider);
+  }
+
+  @Bean
+  public ChatModelCompletionContentObservationFilter chatModelCompletionContentObservationFilter() {
+    return new ChatModelCompletionContentObservationFilter();
+  }
+
+  @Service
+  public static class McpAsyncClientInitializer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(McpAsyncClientInitializer.class);
+    private final AsyncMcpToolCallbackProvider mcpToolCallbackProvider;
+
+    public McpAsyncClientInitializer(final AsyncMcpToolCallbackProvider mcpToolCallbackProvider) {
+      this.mcpToolCallbackProvider = mcpToolCallbackProvider;
+    }
+
+    @Async
+    public void runAsyncTask() {
+      final var toolCallbacks = this.mcpToolCallbackProvider.getToolCallbacks();
+      LOGGER.info(
+          "Found tools: {}",
+          Arrays.stream(toolCallbacks).map(t -> t.getToolDefinition().name()).toList());
+    }
+  }
+}
